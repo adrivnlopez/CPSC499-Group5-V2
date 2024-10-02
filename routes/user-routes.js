5c1572fddb773185158131d3deb98e40c13854bd
Youssef Hegazy <You7Joe@iCloud.com>
2024-10-02 16:18:37 -0700

    added a new route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,15 +4,35 @@ import
 {
     CreateUser,
     GetUser,
-    UpdateUser,
+    UpdateUserInfo,
     DeleteUser
 }from "../controllers/user-controller";
 
-const UserRoute = Router();
+const ClientRoute = Router();
 
-UserRoute.post("/", CreateUser);
-UserRoute.get("/", GetUser);
-UserRoute.put("/", UpdateUser);
-UserRoute.delete("/", DeleteUser);
+ClientRoute.post("/", CreateUser);
 
-export default UserRoute;
+ClientRoute.get("/", GetUser); 
+
+ClientRoute.get("/", async (req,res) =>  // get the user name from the email address.
+    {
+        try 
+        {
+            const UserEmailToCheck = req.params.email; // get the email from the request.
+            const PossibleUserName = await User.find({UserEmailToCheck}); // find the user with the email.
+            res.status(200).json(PossibleUserName); // if Successful, send the UserName back to the client.
+        } 
+        catch (err) // if there is an error, send a message to the client.
+        {
+            console.log(err); // log the error to the console.
+            res.json({"status": "Email Address not found."}); // send the message to the client.
+        }
+    });
+
+
+    ClientRoute.put("/", UpdateUserInfo) // update the user information.
+
+
+ClientRoute.delete("/", DeleteUser);
+
+export default ClientRoute;
