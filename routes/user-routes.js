64f86a0175500a6fdf59238a086ca91260b6223b
Youssef Hegazy <You7Joe@iCloud.com>
2024-10-02 16:18:02 -0700

    finished the ClientRoute for getting the username through the Email Address

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,18 @@
-import { Router } from "express";
+import Router from "express";
 
-import (
-    createUser,
-    getUser,
-    updateUser,
-    deleteUser
-)from "../controllers/user-controller";
+import 
+{
+    CreateUser,
+    GetUser,
+    UpdateUser,
+    DeleteUser
+}from "../controllers/user-controller";
+
+const UserRoute = Router();
+
+UserRoute.post("/", CreateUser);
+UserRoute.get("/", GetUser);
+UserRoute.put("/", UpdateUser);
+UserRoute.delete("/", DeleteUser);
+
+export default UserRoute;
