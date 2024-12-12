ced497fb290b5ea2efec662a5f405c86305c5ba3
Sebastian <serranosebastian23@gmail.com>
2024-12-12 13:59:18 -0800

    added deleteProfile to profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { protect } = require('../middlewares/authMiddleware');       // middleware for authentication
-const { getProfile, updateProfile } = require('../controllers/profileController');
+const { deleteProfile, getProfile, updateProfile } = require('../controllers/profileController');
 const router = express.Router();
 
 // general user profile routes
 router.get('/:userId', protect, getProfile);        // get user profile
 router.put('/:userId', protect, updateProfile);     // update user profile
+router.put('/:userId', protect, deleteProfile);     // delete user Profile
 
 module.exports = router;
