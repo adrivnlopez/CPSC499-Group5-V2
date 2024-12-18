b2ff8335aa63bb44786ed9c659533e5a40728903
adrivnlopez <adrivnlopez@csu.fullerton.edu>
2024-12-17 18:56:26 -0800

    BACKEND_URL

---
+++ BackEnd/routes/goalTrackingRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { protect } = require("../middlewares/authMiddleware"); // Middleware for authentication
+const {
+  getGoals,
+  createGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalTrackerController");
+const router = express.Router();
+
+// route to fetch the goals for a user
+router.get("/user/:userId/goals", protect, getGoals);
+// route to create a new goal for the user
+router.post("/user/:userId/goals", protect, createGoal);
+//route to update an existing goal
+router.put("/goals/:goalId", protect, updateGoal);
+// route to delete an existing goal
+router.delete("/goals/:goalId", protect, deleteGoal);
+
+module.exports = router;
